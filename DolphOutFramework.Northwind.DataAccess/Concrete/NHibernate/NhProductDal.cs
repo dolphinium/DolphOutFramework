4258d844ac6ac636730c529eae86c790b2709521
dolphinium <dolphofficial@gmail.com>
2022-03-05 16:19:37 +0300

    Basic Repository Implementations and Mapping Operations for NHibernate. I tested the database connectivity via Unit Test Class and all done!

---
+++ DolphOutFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DolphOutFramework.Core.DataAccess.NHibernate;
+using DolphOutFramework.Northwind.DataAccess.Abstract;
+using DolphOutFramework.Northwind.Entities.Concrete;
+
+namespace DolphOutFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhProductDal:NhEntityRepositoryBase<Product>,IProductDal
+    {
+        public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
