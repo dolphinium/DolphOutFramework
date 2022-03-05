27d881538522d3e284b25054e2b123a4257af4a3
dolphinium <dolphofficial@gmail.com>
2022-03-05 03:02:49 +0300

    Basic Repository Implementations and Mapping Operations(manual for now, will be using libs later). I tested the methods of EfProductDal and all done!

---
+++ DolphOutFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DolphOutFramework.Core.DataAccess.EntityFramework;
+using DolphOutFramework.Northwind.DataAccess.Abstract;
+using DolphOutFramework.Northwind.Entities.Concrete;
+
+namespace DolphOutFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal:EfEntityRepositoryBase<Product,NorthwindContext> ,IProductDal
+    {
+       
+    }
+}
