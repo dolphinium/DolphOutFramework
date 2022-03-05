0742299225f453018c32d63e68ca967901f93d04
dolphinium <dolphofficial@gmail.com>
2022-03-05 22:06:37 +0300

    ProductManager and IProductService classes and operations has been added for business layer.

---
+++ DolphOutFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DolphOutFramework.Northwind.Entities.Concrete;
+
+namespace DolphOutFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ DolphOutFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DolphOutFramework.Northwind.Business.Abstract;
+using DolphOutFramework.Northwind.DataAccess.Abstract;
+using DolphOutFramework.Northwind.Entities.Concrete;
+
+namespace DolphOutFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+    }
+}
