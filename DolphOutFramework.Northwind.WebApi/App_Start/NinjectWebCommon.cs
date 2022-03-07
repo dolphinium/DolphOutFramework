9ab30390edee122134f671db2f2659189696d175
dolphinium <dolphofficial@gmail.com>
2022-03-07 22:33:05 +0300

    WebApi Authentication and Role Based Authentication handled. Tested with Postman.

--- DolphOutFramework.Northwind.WebApi/App_Start/NinjectWebCommon.cs
+++ DolphOutFramework.Northwind.WebApi/App_Start/NinjectWebCommon.cs
@@ -1,21 +1,19 @@
+using System;
+using System.Web;
 using System.Web.Http;
 using DolphOutFramework.Northwind.Business.Dependency.Resolvers.Ninject;
+using DolphOutFramework.Northwind.WebApi;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
 using WebApiContrib.IoC.Ninject;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(DolphOutFramework.Northwind.WebApi.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(DolphOutFramework.Northwind.WebApi.App_Start.NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
 
-namespace DolphOutFramework.Northwind.WebApi.App_Start
+namespace DolphOutFramework.Northwind.WebApi
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-    using Ninject.Web.Common.WebHost;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
--- DolphOutFramework.Northwind.WebApi/App_Start/WebApiConfig.cs
+++ DolphOutFramework.Northwind.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using DolphOutFramework.Northwind.WebApi.MessageHandlers;
 
 namespace DolphOutFramework.Northwind.WebApi
 {
@@ -10,6 +11,7 @@ namespace DolphOutFramework.Northwind.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.MessageHandlers.Add(new AuthenticationHandler());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
